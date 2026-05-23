f5771f65d3e893c26480d47ab542db780c774b85
Jose-Serrano1 <josedsrobles@gmail.com>
2026-05-23 03:32:57 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(5200, () => {
+  console.log("Server running on port 5200");
+});
